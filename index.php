ab1138404bd6de81dec72e6a21a8b64474654660
Perla Kozhaya <kozhaya.perla@gmail.com>
2023-04-28 23:59:40 +0300

    Linked Font Awesome to index.php

--- index.php
+++ index.php
@@ -5,19 +5,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link href="assets/css/styles.css" rel="stylesheet" type="text/css">
     
     <link rel="apple-touch-icon" sizes="180x180" href="assets/favicon/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon/favicon-16x16.png">
     <link rel="manifest" href="assets/favicon/site.webmanifest">
 
+    <link href="assets/css/styles.css" rel="stylesheet" type="text/css">
+
+    <link href="assets/font-awesome/css/all.css" rel="stylesheet">
+
     <title>Property Rental & Listing | HomeHive</title>
 </head>
 
 <body>
-    
+    <i class="fa-solid fa-chevron-left"></i>
+    <i class="fa-solid fa-chevron-right"></i>
 </body>
 
 </html>
